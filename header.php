0c46528d6df523452efa750d49a0afc84397f978
unknown <gyebnarpetter@gmail.com>
2020-08-04 12:26:03 +0200

    create basic files

---
+++ header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="ajándék, karácsony, lánybúcsú, legénybúcsú, mr és mrs, póló, vicces, születésnap, szülinap, póló, bögre, bakelilemez óra, egyedi párna, egyedi, saját fényképes, bakelit óra, kisvakond póló, sebességkorlátozó tábla, szülinapi kresztábla">
+    <meta name="description" content="Egyedi ajándékok, vicces, szülinapi, feliratos pólók nőknek, férfiaknak, pólók legénybúcsúra és lánybúcsúra. Gyerekpóló, baba body. Egyedi és vicces bögrék, párnák. Bakelitlemez órák gyártása és tervezése. Sebességkorlátozó tábla születésnapra.">
+    <link rel="stylesheet" href="./css/main.css">
+    <title>Ajándékműhely - Az ajándék birodalom</title>
+    <script src="https://kit.fontawesome.com/ca795931b7.js" crossorigin="anonymous"></script>
+</head>
+<body>
+
+<section class="header-wrap">
+    <header>
+        <div class="header-top">
+
+            <div class="header-logo">
+                <img src="./img/ajandekmuhely-logo.png" alt="ajandekmuhely-logo">
+            </div>
+
+            <div class="search">
+                <input type="text" name="search" id="search" placeholder="Termék neve">
+                <i class="fas fa-search"></i>
+            </div>
+
+            <div class="shopping-cart">
+                <i class="fas fa-shopping-cart"></i>
+                <div class="shopping-sum">
+                    <span class="quantity">0</span> db / <span class="price">0</span> ft
+                </div>
+            </div>
+
+        </div>
+        <nav></nav>
+    </header>
+</section>
+    
